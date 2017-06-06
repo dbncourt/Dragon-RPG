ced61d583d84877c62ea6cebe462e0dc6d999052
Daniel Betancourt <dbncourt@gmail.com>
2017-06-05 18:44:03 -0600

    006 - A Simple DIY Follow Camera. Set tags for player and camera. Added CameraFollow.cs script to enable the main camera to follow the player.

---
+++ Dragon RPG/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+        {
+            Debug.LogError("No player to follow was found");
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
