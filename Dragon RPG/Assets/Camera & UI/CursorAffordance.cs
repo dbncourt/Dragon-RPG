8fd4e4185ed10caa339b03444db818452003faff
Daniel Betancourt <dbncourt@gmail.com>
2017-06-06 21:18:25 -0600

    015 - Using Cursor Affordances Set the cursor to change depending on what is hovering. Updated the enemies prefab layer to Enemies, also set the tag to untagged.

---
+++ Dragon RPG/Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D walkCursor;
+
+    [SerializeField]
+    private Texture2D targetCursor;
+
+    [SerializeField]
+    private Texture2D unknownCursor;
+
+    [SerializeField]
+    private Vector2 cursorHotspot;
+
+    private CameraRaycaster cameraRaycaster;
+
+    public CursorAffordance()
+    {
+        walkCursor = null;
+        targetCursor = null;
+        unknownCursor = null;
+        cursorHotspot = new Vector2(96, 96);
+    }
+    
+    // Use this for initialization
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                break;
+        }
+    }
+}
