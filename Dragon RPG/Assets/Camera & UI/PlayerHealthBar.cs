13d2f5518dcf64fae5e1d70759720a46125b9193
Daniel Betancourt <dbncourt@gmail.com>
2017-06-08 22:59:03 -0600

    029 - Player Health Bar UI
    Added a healthbar to the ui.

---
+++ Dragon RPG/Assets/Camera & UI/PlayerHealthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class PlayerHealthBar : MonoBehaviour
+{
+    private RawImage healthBarRawImage;
+    private Player player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthBarRawImage = GetComponent<RawImage>();
+    }
+
+    void Update()
+    {
+        float xValue = -(player.healthAsPercentage / 2f) - 0.5f;
+        healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
---
+++ Dragon RPG/Assets/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float maxHealthPoints;
+    private float currentHealthPoints;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    public Player()
+    {
+        maxHealthPoints = 100.0f;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealthPoints = maxHealthPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
