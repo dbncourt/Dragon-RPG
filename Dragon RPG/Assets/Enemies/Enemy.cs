cb1cb56ce2de186a63b92cb9ed467285a971fbbf
Daniel Betancourt <dbncourt@gmail.com>
2017-06-10 17:23:30 -0600

    030 - Enemy Floating Health Bar
    Added healthbar for enemy prefab.

---
+++ Dragon RPG/Assets/Enemies/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private float maxHealthPoints;
+    private float currentHealthPoints;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    public Enemy()
+    {
+        maxHealthPoints = 100.0f;
+    }
+
+    void Start()
+    {
+        currentHealthPoints = maxHealthPoints;
+    }
+
+    void Update()
+    {
+
+    }
+}
