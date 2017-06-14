0c8ab089fa3b2ff633fbc692c1d3ed22cea409fa
Daniel Betancourt <dbncourt@gmail.com>
2017-06-14 17:29:05 -0600

    043 - Make Enemies Pathfind To Player
    Implemented path finding for the enemies to the player.

--- Dragon RPG/Assets/Enemies/Enemy.cs
+++ Dragon RPG/Assets/Enemies/Enemy.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
-
+using UnityStandardAssets.Characters.ThirdPerson;
 public class Enemy : MonoBehaviour
 {
 
     [SerializeField]
     private float maxHealthPoints;
+    [SerializeField]
+    private float attackRadius = 4.0f;
+
     private float currentHealthPoints;
+    private GameObject player;
+    private AICharacterControl aiCharacterControl;
 
     public float healthAsPercentage
     {
@@ -18,15 +23,28 @@ public class Enemy : MonoBehaviour
     public Enemy()
     {
         maxHealthPoints = 100.0f;
+        player = null;
+        aiCharacterControl = null;
     }
 
     void Start()
     {
         currentHealthPoints = maxHealthPoints;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
     }
 
     void Update()
     {
-
+        float distanceToPlayer = Vector3.Distance(player.transform.position, aiCharacterControl.transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
     }
 }
